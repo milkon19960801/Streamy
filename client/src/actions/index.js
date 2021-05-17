ccf3c8821bb9df810192a7ad36e6803f29ccc042
milkon <melkon_2010@hotmail.com>
2021-05-17 17:28:21 +0200

    added some types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
