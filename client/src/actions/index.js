a96774b13a77f46ecc879144ec039a40de10e8a9
milkon <melkon_2010@hotmail.com>
2021-05-17 19:43:08 +0200

    added stream reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -30,3 +30,27 @@ export const createStream = (formValues) => async (dispatch) => {
     payload: response.data,
   });
 };
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
