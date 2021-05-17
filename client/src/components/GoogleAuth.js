f145f2ee6fd9d5d3bfc415d4beb8fe8176396872
milkon <melkon_2010@hotmail.com>
2021-05-17 12:29:55 +0200

    code refactor

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -8,7 +8,7 @@ class GoogleAuth extends React.Component {
       window.gapi.client
         .init({
           clientId:
-            "797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com",
+            "104518113324-ns5e8quhg3t8gki0ganp43r15hava8i1.apps.googleusercontent.com",
           scope: "email",
         })
         .then(() => {
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  render() {
+    return <div>StreamCreate</div>;
+  }
+}
 
 export default StreamCreate;
