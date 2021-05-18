a897b66930a6ed860f9914341a0b6c6c5b1386d9
milkon <melkon_2010@hotmail.com>
2021-05-18 15:39:22 +0200

    Implementing Programmatic Navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -33,6 +34,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     type: CREATE_STREAM,
     payload: response.data,
   });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
